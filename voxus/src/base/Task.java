260ea92a7b6081c205deb587b2883769017d4ae4
ph-pc <ph-pc@Quarto.home>
2017-11-14 12:36:22 -0200

    servlet, primeiro controller, task

---
+++ voxus/src/base/Task.java
@@ -0,0 +1,15 @@
+package base;
+
+public class Task {
+	
+	private String titulo;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+}
