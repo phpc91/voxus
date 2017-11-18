80f10915d8bfafabd30f607702468365e9e24f32
ph-pc <ph-pc@Quarto.home>
2017-11-18 00:12:38 -0200

    controller de criação de task

--- voxus/src/controller/CriarController.java
+++ voxus/src/controller/CriarController.java
@@ -1,16 +1,25 @@
 package controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import dao.TaskDAO;
 
 /**
  * Servlet implementation class CriarController
  */
 @WebServlet("/criar")
+@MultipartConfig
 public class CriarController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -32,7 +41,32 @@ public class CriarController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		String nome = request.getParameter("titulo");
+		String desc = request.getParameter("descricao");
+		int prioridade = Integer.parseInt(request.getParameter("prioridade"));
+		TaskDAO dao = TaskDAO.getInstance();
+		dao.createTask(nome);
+		
+	    List<Part> fileParts = 
+	    		request.getParts().stream().filter(part -> "file".equals(part.getName())).collect(Collectors.toList());
+
+	    for (Part filePart : fileParts) {
+	        String fileName = getSubmittedFileName(filePart);
+	        InputStream fileContent = filePart.getInputStream();
+	        //TODO implementar inserção no bd
+	    }
+	    
+	    request.getRequestDispatcher("/sucesso").forward(request, response);
+	}
+	
+	private static String getSubmittedFileName(Part part) {
+	    for (String cd : part.getHeader("content-disposition").split(";")) {
+	        if (cd.trim().startsWith("filename")) {
+	            String fileName = cd.substring(cd.indexOf('=') + 1).trim().replace("\"", "");
+	            return fileName.substring(fileName.lastIndexOf('/') + 1).substring(fileName.lastIndexOf('\\') + 1);
+	        }
+	    }
+	    return null;
 	}
 
 }
